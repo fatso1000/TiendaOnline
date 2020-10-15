26a533b53ebce522d2430185937a7c82d5e47afc
fatso1000 <agustinbenitez81@gmail.com>
2020-10-15 08:58:09 -0300

    Eliminadas contraseñas filtradas

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
         connectionLimit: 10,
         host: process.env.DATABASE_HOST || 'localhost',
         user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || 'poderOS123',
+        password: process.env.DATABASE_PASSWORD || '',
         database: process.env.DATABASE_NAME || 'darlingphotos'
     }
 }
